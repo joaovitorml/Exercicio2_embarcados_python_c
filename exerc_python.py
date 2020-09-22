aaa5686e19654bc965bb9b874efe7a7bfcd82294
joaovitorml <joao.lemos4000@gmail.com>
2020-09-22 10:53:35 -0300

    Testing python code

--- exerc_python.py
+++ exerc_python.py
@@ -19,10 +19,14 @@ print("Press  o atmosf  rica: " + str(dado.pressure))
 
 mylcd = RPi_I2C_driver.lcd()
 
+temperatura = str("{:.2f}".format(dado.temperature))
+pressao = str("{:.2f}".format(dado.pressure))
+umidade = str("{:.2f}".format(dado.humidity))
+
 while True:
-    mylcd.lcd_display_string(str("{:.2f}".format(dado.temperature),1) + str("{:.2f}".format(dado.pressure),1))
-    mylcd.lcd_display_string(str("{:.2f}".format(dado.temperature)), 2)
+    mylcd.lcd_display_string("T="+temperatura+" P="+pressao,1)
+    mylcd.lcd_display_string("Umi="+umidade, 2)
     sleep(1)
-    mylcd.lcd_clear()
+
 
 
